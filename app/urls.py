3c40357c6f5473fb233119738cb53f3085329758
wxysch <uwu1343@gmail.com>
2024-01-08 01:26:28 +0600

    Routing URLs - include, namespace

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from main import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name='index'),
-    path('about/',views.about,name='about'),
+    path('', include('main.urls', namespace='main')),
 ]
